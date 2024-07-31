3ea2a472f71864185ea16b724e67e344235a5d62
Nokwanda2000 <nokwandachonco262@gmail.com>
2024-07-31 16:01:10 +0200

    new

--- my_card_example/src/components/card.jsx
+++ my_card_example/src/components/card.jsx
@@ -1,18 +1,20 @@
 import { CiHeart } from "react-icons/ci";
 import { IoBedOutline } from "react-icons/io5";
 import { PiBathtubLight } from "react-icons/pi";
+import Hover from "./hover";
 
 function Card(){
 return(
     <>
      <div className="main" style={{
-      backgroundColor:"red",
-      width:"500px",
-      height:"700px",
+    //   backgroundColor:"red",
+      width:"470px",
+      height:"600px",
       borderRadius:"10px",
-      boxShadow:"10px 10px  rgb(230, 230, 230)",
+      boxShadow:"10px 10px  rgb(230, 230, 240)",
       rowGap:"none",
-      gap:"none"
+      gap:"none",
+    
     
         
       
@@ -28,30 +30,45 @@ return(
             borderTopRightRadius:"10px",
             borderTopLeftRadius:"10px"
         }}>
-
+{/* 
 <CiHeart className="icon" style={{
-    fontSize:"30px",
-    color:"white",
-    marginRight:"-290px",
+    fontSize:"40px",
+    color:"red",
+    marginRight:"-400px",
     transform: "translate(-10%, -10%)",
     cursor:"pointer",
+    marginBottom:"-40px"
     
-}}/> 
+}}/>  */}
+<Hover style={{
 
+
+}}/>
      </div>   
 
         <div className="2" style={{
             backgroundColor:"white",
             width:"100%",
             height:"30%",
+            
           
         }}
     
         >
             
-          <h3>DETACHED HOUSE . 5Y OLD</h3 >  
-          <h1>$750,000</h1>
-          <h3>742 Evergreen Terrance</h3>
+          <span style={{
+            marginRight:"200px"
+          }}
+          >DETACHED HOUSE . 5Y OLD</span> 
+
+          <h3 style={{
+            marginRight:"330px"
+          }}
+          >$750,000</h3>
+
+          <h3 style={{
+            marginRight:"240px"
+          }}>742 Evergreen Terrance</h3>
 
         </div>
         
@@ -62,7 +79,7 @@ return(
             height:"15%"
 
         }}>
-            <h4><IoBedOutline />3 Bedroom         <PiBathtubLight />  2Bathrooms</h4> 
+            <span><IoBedOutline />3 Bedroom       <PiBathtubLight />  2 Bathrooms</span>
 
         </div>
 
@@ -72,10 +89,33 @@ return(
             height:"15%",
             borderBottomRightRadius:"10px",
             borderBottomLeftRadius:"10px",
+            
         }}>
             
-            <h4>REALTOR</h4>
+            <span style={{
+                marginRight:"300px"
+            }}>REALTOR</span>
 
+
+
+
+
+
+           
+            <span>
+            <img style={{
+            width:"50px",
+            height:"50px",
+            borderRadius:"50%",
+            marginRight:"240px"
+           }}
+           src="./src/assets/GIRL.jpg" ></img>
+
+
+
+           Tiffany Heffner
+           {/* <p>(555) 555- 4321</p> */}
+           </span>
         </div>
 
      </div>
--- my_card_example/src/components/hover.jsx
+++ my_card_example/src/components/hover.jsx
@@ -1,15 +1,56 @@
 import { CiHeart } from "react-icons/ci";
 import { useState } from "react";
+
 function Hover(){
 
     
-//    const[icon,setIcon] = useState("")
+
+// let isLiked = false;
+
+
+let IconColor = "red";
+
+const [isLiked, setIsLiked] = useState(true)
+
+
+function ChangeLike(){
+
+
+    if(isLiked){
+
+        
+        setIsLiked(false);
+        IconColor= "white";
+
+        console.log("Liked!", )
+
+        
+    };
+
+   
+
     
+    if(!isLiked){
+
+        setIsLiked(true);
+        IconColor = "red";
+        
+        console.log("Not Liked!")
+
+    };
+
+   
+    
+
+
+}
+
+
 
     return(
         <>
 
-<CiHeart/> 
+       <CiHeart className="icon" size={40} color={IconColor} onClick={ChangeLike}/> 
         </>
     )
 
